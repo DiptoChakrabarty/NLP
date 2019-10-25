053a458d61763f23ab34d4b933019c75d0a67ffe
Dipto Chakrabarty <diptochuck123@gmail.com>
2019-10-25 12:00:55 +0530

    cleaning/training

--- spam.py
+++ spam.py
@@ -11,4 +11,14 @@ from nltk.stem.porter import PorterStemmer
 ps=PorterStemmer()
 corpus=[]
 for i in range(len(msg)):
-    
+    review=re.sub('[^a-zA-Z]',' ',msg['text'][i])
+    review=review.lower()
+    review=review.split()
+    review=[ps.stem(word) for word in review if word not in set(stopwords.words("english"))]    
+    review= " ".join(review)
+    corpus.append(review)
+print(corpus)
+
+from sklearn.feature_extraction.text import CountVectorizer
+cv=CountVectorizer(max_features=3000)
+X=cv.fit_transform(corpus).toarray()
